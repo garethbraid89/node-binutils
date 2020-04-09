656035d73202cfb5dade4c328ef6c5f6d23246ef
gareth <garethbraid89@gmail.com>
2020-04-09 20:18:39 +0100

    Changed var to const/let

--- binutils.js
+++ binutils.js
@@ -20,98 +20,97 @@ module.exports = class BinaryReader {
         this.Position = 0;
     }
 
-    ReadUInt8 = function() {
+    ReadUInt8 = function () {
         if (this.ByteBuffer.length < 1) {
             return 0;
         }
 
-        var s_Val = this.ByteBuffer.readUInt8(this.Position);
+        const s_Val = this.ByteBuffer.readUInt8(this.Position);
         ++this.Position;
         return s_Val;
     }
 
-    ReadUInt16 = function() {
+    ReadUInt16 = function () {
         if (this.ByteBuffer.length < 2) {
             return 0;
         }
 
-
-        var s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readUInt16LE(this.Position) : this.ByteBuffer.readUInt16BE(this.Position);
+        const s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readUInt16LE(this.Position) : this.ByteBuffer.readUInt16BE(this.Position);
         this.Position += 2;
         return s_Val;
     }
 
-    ReadUInt32 = function() {
+    ReadUInt32 = function () {
         if (this.ByteBuffer.length < 4) {
             return 0;
         }
 
-        var s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readUInt32LE(this.Position) : this.ByteBuffer.readUInt32BE(this.Position);
+        const s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readUInt32LE(this.Position) : this.ByteBuffer.readUInt32BE(this.Position);
         this.ByteBuffer = this.ByteBuffer.slice(4);
         this.Position += 4;
         return s_Val;
     }
 
-    ReadInt8 =  function() {
+    ReadInt8 = function () {
         if (this.ByteBuffer.length < 1) {
             return 0;
         }
 
-        var s_Val = this.ByteBuffer.readInt8(this.Position);
+        const s_Val = this.ByteBuffer.readInt8(this.Position);
         ++this.Position;
         return s_Val;
     }
 
-    ReadInt16 = function() {
+    ReadInt16 = function () {
         if (this.ByteBuffer.length < 2) {
             return 0;
         }
 
-        var s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readInt16LE(this.Position) : this.ByteBuffer.readInt16BE(this.Position);
+        const s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readInt16LE(this.Position) : this.ByteBuffer.readInt16BE(this.Position);
         this.ByteBuffer = this.ByteBuffer.slice(2);
         this.Position += 2;
         return s_Val;
     }
 
-    ReadInt32 = function() {
+    ReadInt32 = function () {
         if (this.ByteBuffer.length < 4) {
             return 0;
         }
 
-        var s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readInt32LE(this.Position) : this.ByteBuffer.readInt32BE(this.Position);
+        const s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readInt32LE(this.Position) : this.ByteBuffer.readInt32BE(this.Position);
         this.ByteBuffer = this.ByteBuffer.slice(4);
         this.Position += 4;
         return s_Val;
     }
 
-    ReadFloat = function() {
+    ReadFloat = function () {
         if (this.ByteBuffer.length < 4) {
             return 0.0;
         }
 
-        var s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readFloatLE(this.Position) : this.ByteBuffer.readFloatBE(this.Position);
+        const s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readFloatLE(this.Position) : this.ByteBuffer.readFloatBE(this.Position);
         this.ByteBuffer = this.ByteBuffer.slice(4);
         this.Position += 4;
         return s_Val;
     }
 
-    ReadDouble = function() {
+    ReadDouble = function () {
         if (this.ByteBuffer.length < 8) {
             return 0.0;
         }
 
-        var s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readDoubleLE(this.Position) : this.ByteBuffer.readDoubleBE(this.Position);
+        const s_Val = (this.Endianness === 'little') ? this.ByteBuffer.readDoubleLE(this.Position) : this.ByteBuffer.readDoubleBE(this.Position);
         this.ByteBuffer = this.ByteBuffer.slice(8);
         this.Position += 8;
         return s_Val;
     }
 
-    ReadBytes = function(p_Count) {
+    ReadBytes = function (p_Count) {
         if (p_Count > this.ByteBuffer.length) {
-            return  Buffer.alloc(0);
+            return Buffer.alloc(0);
         }
 
-        var s_Val = Buffer.from(this.ByteBuffer, this.Position, p_Count);
+        const s_Val = Buffer.from(this.ByteBuffer, this.Position, p_Count);
         // this.ByteBuffer.copy(s_Val, 0, 0, p_Count);
         //
         // this.ByteBuffer = this.ByteBuffer.slice(p_Count);
